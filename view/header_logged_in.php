83395be8f7f2c60c5e165b1ec0416d2662e09dc6
Gassmann Flynn <bgassf@bbc-be.local>
2018-09-19 12:58:51 +0200

    Farben JJJJAAAAAAA

--- view/header_logged_in.php
+++ view/header_logged_in.php
@@ -27,7 +27,7 @@
     ?>
 
   </head>
-  <body>
+  <body class="<?= $pageColor ?>">
     <nav class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
